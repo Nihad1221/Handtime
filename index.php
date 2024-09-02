8806da011f9dc0ee2efe2f4f35148282090db8e1
Nihad1221 <babayevnihad.6@gmail.com>
2024-09-02 20:08:58 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include_once './config/index.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    include_once 'views/includes/head.php';
+    ?>
+</head>
+
+<body>
+<?php
+include_once 'views/includes/header.php';
+include_once 'views/includes/js.php';
+include_once 'views/includes/footer.php';
+
+include_once 'views/pages/service.php';
+include_once 'views/pages/about.php';
+include_once 'views/pages/product.php';
+include_once 'views/pages/contact.php';
+include_once 'views/pages/service.php';
+include_once 'views/pages/info.php';
+?>
+
+</body>
+
+</html>
---
+++ views/includes/head.php
@@ -0,0 +1,27 @@
+<!-- Basic -->
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+<!-- Site Metas -->
+<link rel="icon" href="<?= DOMAIN ?>/assets/images/fevicon/fevicon.png" type="image/gif"/>
+<meta name="keywords" content=""/>
+<meta name="description" content=""/>
+<meta name="author" content=""/>
+
+<title>HandTime</title>
+
+
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="<?= DOMAIN ?>/assets/css/bootstrap.css"/>
+
+<!-- fonts style -->
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+<!-- font awesome style -->
+<link href="<?= DOMAIN ?>/assets/css/font-awesome.min.css" rel="stylesheet"/>
+
+<!-- Custom styles for this template -->
+<link href="<?= DOMAIN ?>/assets/css/style.css" rel="stylesheet"/>
+<!-- responsive style -->
+<link href="<?= DOMAIN ?>/assets/css/responsive.css" rel="stylesheet"/>
